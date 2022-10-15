b98e2bb89e5c793697292913d149b40c29f6a665
mhasan5995 <mhasan@dairymaster.com>
2022-10-15 16:49:14 +0100

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Time-calculator';
+  startdate = new Date();
+  enddate = new Date();
+  name = 'Hi';
+  TotalDays='';
+  calculate(){
+    this.TotalDays= '';
+    console.log('clicked');
+    let difference = this.enddate.getTime() - this.startdate.getTime();
+    let Days = Math.ceil(difference / (1000 * 3600 * 24));
+    console.log(Days);
+   this.TotalDays = this.TotalDays + Days;
+  } 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
